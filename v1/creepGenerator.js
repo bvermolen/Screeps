2a05fafbf3bf20b6fca0e4c42b8aebb08a7708cf
Bas Vermolen <bas@vermolen.me>
2015-02-20 16:49:16 +0100

    Update creepGenerator.js

--- v1/creepGenerator.js
+++ v1/creepGenerator.js
@@ -28,8 +28,6 @@
                     console.log('Start spawning ' + cType+": "+result);
                     creepTypes[cType]++;
                     break;
-                } else {
-                    console.log('Could not start spawning ' + creepRole+": "+result);
                 }
             }
         }
